a212b8eb5808d7c9bceb56301791089e3636abe8
szymonszymonszymonv <szymon.prokopiuk99@gmail.com>
2022-03-27 16:14:01 +0200

    add vigenere and matrixC decoding

--- kryptografia/src/MatrixC.js
+++ kryptografia/src/MatrixC.js
@@ -1,9 +1,12 @@
+import { Switch } from '@mui/material';
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+
 function MatrixC() {
     const [input, setInput] = useState('')
-    const [key, setKey] = useState('')
+    const [key, setKey] = useState('convenience')
     const [encoded, setEncoded] = useState('')
+    const [isDecipher, setIsDecipher] = useState(false)
 
     const columnOrder = (key) => {
         const sortedKey = key.replace(/\s/g, '').split('').sort()
@@ -24,24 +27,8 @@ function MatrixC() {
         return order
     }
 
-    const matrixCipher = (input, key) => {
-        input = input.replace(/\s/g, '')
-        let order = columnOrder(key)
-        let matrix = []
-        let encodedResult = ""
-        const inputLen = input.length
-        let cursor = 0 // current index
-        for(let num of order) {
-            let word = []
-            for(let i = 0; (i < num && cursor < inputLen); i++){
-                word.push(input[cursor])
-                cursor++
-            }
-            matrix.push(word)
-            if(cursor >= inputLen){
-                break
-            }
-        }
+    const encoding = (matrix, order) => {
+        let result = ""
         for(let col of order){
             let newWord = ""
             for(let row of matrix) {
@@ -50,16 +37,72 @@ function MatrixC() {
             if(!newWord) {
                 continue
             }
-            encodedResult += newWord + " "
+            result += newWord + " "
         }
+        return result
+    }
+
+    const matrixDecipher = (text, key) => {
+        const keyLen = key.length
+        let order = columnOrder(key)
+        let matrix = new Array(keyLen)
+        let textArr = text.split(" ")
+        let encodedResult = ""
+        for(let i = 0; i < keyLen; i++) {
+            let idx = order.indexOf(i + 1)
+            matrix[i] = textArr[idx]
+        }
+        for(let num of order) {
+            let currentOrder = num
+            while(currentOrder > 0){
+                for(let i = 0; i < matrix.length; i++) {
+                    encodedResult += matrix[i][0] ?? ''
+                    matrix[i] = matrix[i].slice(1)
+                    currentOrder--
+                    if(currentOrder <= 0){
+                        break
+                    }
+                }
+            }
+        }
+        setEncoded(encodedResult)
+    }
+
+    const matrixCipher = (text, key) => {
+        if(isDecipher) {
+            matrixDecipher(encoded, key)
+            return
+        }
+        text = text.replace(/\s/g, '')
+        let order = columnOrder(key)
+        let matrix = []
+        let encodedResult = ""
+        const inputLen = text.length
+        let cursor = 0 // current index
+        for(let num of order) {
+            let word = []
+            for(let i = 0; (i < num && cursor < inputLen); i++){
+                word.push(text[cursor])
+                cursor++
+            }
+            matrix.push(word)
+            if(cursor >= inputLen){
+                break
+            }
+        }
+        encodedResult = encoding(matrix, order)
         setEncoded(encodedResult)
     }
     return (
         <div className='form'>
+            <div className='switch-wrapper'>
+                <Switch checked={isDecipher} onChange={(e) => { setIsDecipher(e.target.checked)}} color="secondary"></Switch>
+                <label>Deszyfrowanie</label>
+            </div>
             <label>Tekst</label>
             <input type="text" onChange={(e) => {setInput(e.target.value)}}></input>
             <label>Klucz</label>
-            <input type="text" onChange={(e) => {setKey(e.target.value)}}></input>
+            <input type="text" value={key} onChange={(e) => {setKey(e.target.value)}}></input>
             <button onClick={() => {matrixCipher(input, key)}}>Zaszyfruj</button>
             <label>Wynik</label>
             <textarea disabled id="encoded-text" placeholder="zakodowany tekst" value={encoded}></textarea>
--- kryptografia/src/Vigenere.js
+++ kryptografia/src/Vigenere.js
@@ -1,23 +1,40 @@
+import { Divider, Switch } from '@mui/material';
 import React, { useState } from 'react';
+import './styles/Vigenere.css'
 function Vigenere() {
     const [input, setInput] = useState('')
     const [key, setKey] = useState('')
     const [encoded, setEncoded] = useState('')
-    const vigCipher = (input, key) => {
+    const [isDecipher, setIsDecipher] = useState(false)
+    
+    const vigCipher = (text, key) => {
+        if(isDecipher){
+            text = encoded
+        }
+        text.toLowerCase()
         let keyIdx = 0
         let keyLen = key.length
         let encodedResult = ""
         let shift = 0
         const base = ('z'.charCodeAt() - 'a'.charCodeAt() + 1)
-        let inputLen = input.length
+        let inputLen = text.length
         for(let i = 0; i < inputLen; i++) {
             const regex = /^[a-zA-Z]*$/
-            if(!regex.test(input[i])) { 
-                encodedResult += input[i]
+            if(!regex.test(text[i])) { 
+                encodedResult += text[i]
                 continue
             }
             shift = key[keyIdx].toLowerCase().charCodeAt() - 'a'.charCodeAt()
-            let newLetterCode = (input[i].toLowerCase().charCodeAt() - 'a'.charCodeAt() + shift) % base + 'a'.charCodeAt()
+            let newLetterCode = 0
+            if(isDecipher) {
+                newLetterCode = (text[i].charCodeAt() - 'a'.charCodeAt() - shift) % base + 'a'.charCodeAt()
+                if(newLetterCode < 'a'.charCodeAt()) {
+                    newLetterCode += base
+                }
+            }
+            else {
+                newLetterCode = (text[i].charCodeAt() - 'a'.charCodeAt() + shift) % base + 'a'.charCodeAt()
+            }
             encodedResult += String.fromCharCode(newLetterCode)
             keyIdx = (keyIdx + 1) % keyLen
         }
@@ -26,6 +43,10 @@ function Vigenere() {
     }
     return (
         <div className='form'>
+            <div className='switch-wrapper'>
+                <Switch checked={isDecipher} onChange={(e) => { setIsDecipher(e.target.checked)}} color="secondary"></Switch>
+                <label>Deszyfrowanie</label>
+            </div>
             <label>Tekst</label>
             <input type="text" onChange={(e) => {setInput(e.target.value)}}></input>
             <label>Klucz</label>
